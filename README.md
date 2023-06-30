2216ce343ed9eb59c51f0bc19b2d4693486a43bc
Rafael Mateus <rafaelbmateus.dev@gmail.com>
2023-06-30 13:20:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Local Católico - Orações Eurarísticas
+
+Esse projeto tem o objetivo as orações eucarísticas da igreja católica.
---
+++ assets/js/prayers.js
@@ -0,0 +1,32 @@
+fetch("https://raw.githubusercontent.com/rafaelbmateus/localcatolico-oracoes/main/prayers.json")
+  .then((res) => {
+  return res.json();
+})
+.then((data) => prayers(data));
+
+function prayers(data) {
+  let prayers = "";
+  for (var prayer of data.prayers) {
+    if (prayer.enabled != "true"){ continue }
+
+    prayers +=
+      `<div class="accordion-item">
+        <h2 class="accordion-header" id="` + prayer.id + `">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse` + prayer.id + `" aria-expanded="false" aria-controls=flush-collapse"` + prayer.id + `">
+            ` + prayer.name + `<br>
+            ` + prayer.location + `
+          </button>
+        </h2>
+        <div id="flush-collapse` + prayer.id + `" class="accordion-collapse collapse" aria-labelledby="` + prayer.id + `" data-bs-parent="#churchs">
+          <div class="accordion-body">`
+            for (var content of data.content) {
+              prayers += `<p>` + content + `</p>`
+            }
+            prayers +=`
+          </div>
+        </div>
+      </div>`;
+  }
+
+  document.getElementById("prayers").innerHTML = prayers;
+}

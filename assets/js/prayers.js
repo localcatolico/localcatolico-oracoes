2044e672cc4fdb634560c71fa87103bdf7720883
Rafael Mateus <rafaelbmateus.dev@gmail.com>
2023-06-30 13:24:37 -0300

    fix: prayer content and button text

--- assets/js/prayers.js
+++ assets/js/prayers.js
@@ -13,13 +13,12 @@ function prayers(data) {
       `<div class="accordion-item">
         <h2 class="accordion-header" id="` + prayer.id + `">
           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse` + prayer.id + `" aria-expanded="false" aria-controls=flush-collapse"` + prayer.id + `">
-            ` + prayer.name + `<br>
-            ` + prayer.location + `
+            ` + prayer.name + `
           </button>
         </h2>
         <div id="flush-collapse` + prayer.id + `" class="accordion-collapse collapse" aria-labelledby="` + prayer.id + `" data-bs-parent="#churchs">
           <div class="accordion-body">`
-            for (var content of data.content) {
+            for (var content of prayer.content) {
               prayers += `<p>` + content + `</p>`
             }
             prayers +=`
